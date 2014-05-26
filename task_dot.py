0e3bd4b455ba42197cc4523aee84223db19f5d6f
Chrijj <chrijj@gmail.com>
2014-05-26 21:41:21 +0100

    added delete option

--- task_dot.py
+++ task_dot.py
@@ -1,5 +1,6 @@
 # basic task list script
 # currently python 2.7
+# 0.2 - option to remove tasks
 # 0.1 - formatting and consistency updates
 # 0.0 - basic list input
 
@@ -12,11 +13,22 @@ user_input = ""
 
 while user_input != 'e':
 	print '-' * 15
-	user_input = raw_input("Press 'n' to add a new task, 'l' to list tasks or 'e' to exit\n..:")
+	user_input = raw_input("Press 'n' to add a new task, 'd' to delete a task, 'l' to list tasks or 'e' to exit\n..:")
 	if user_input == 'n':
 		user_input = raw_input("Enter the task: ")
 		tasks.append(user_input)
 		print "Task", user_input, "added to task list\n"
+	elif user_input == 'd':
+		if len(tasks) == 0:
+			print "No Tasks in list"
+		else:
+			print "Current Tasks:\n",tasks
+			user_input = raw_input("Task to delete: ")
+			if user_input not in tasks:
+				print "That task is not in the list"
+			else:
+				tasks.remove(user_input)
+				print user_input, "removed from tasks."
 	elif user_input == 'l':
 		if len(tasks) == 0:
 			print "No Tasks in list"
