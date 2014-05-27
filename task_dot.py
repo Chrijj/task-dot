a468763cc51a078f3ce828826f758b77c0e7e8e6
Chrijj <chrijj@gmail.com>
2014-05-27 22:51:23 +0100

    added basic datetime

--- task_dot.py
+++ task_dot.py
@@ -1,14 +1,17 @@
 # basic task list script
 # currently python 2.7
+# 0.3 - changed to make tasks a dictionary with the key being the task name and the value being the date
 # 0.2 - option to remove tasks
 # 0.1 - formatting and consistency updates
 # 0.0 - basic list input
+
+from datetime import date
 
 print '*' * 15
 print "Task List Manager"
 print '*' * 15
 
-tasks = []
+tasks = {}
 user_input = ""
 
 while user_input != 'e':
@@ -16,7 +19,7 @@ while user_input != 'e':
 	user_input = raw_input("Press 'n' to add a new task, 'd' to delete a task, 'l' to list tasks or 'e' to exit\n..:")
 	if user_input == 'n':
 		user_input = raw_input("Enter the task: ")
-		tasks.append(user_input)
+		tasks[user_input] = date.today()
 		print "Task", user_input, "added to task list\n"
 	elif user_input == 'd':
 		if len(tasks) == 0:
@@ -27,7 +30,7 @@ while user_input != 'e':
 			if user_input not in tasks:
 				print "That task is not in the list"
 			else:
-				tasks.remove(user_input)
+				del tasks[user_input]
 				print user_input, "removed from tasks."
 	elif user_input == 'l':
 		if len(tasks) == 0:
