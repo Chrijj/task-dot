c44517393660c2bde96772641986317d88505117
Chrijj <chrijj@gmail.com>
2014-05-29 21:19:40 +0100

    first pass file handling

--- task_dot.py
+++ task_dot.py
@@ -1,6 +1,7 @@
 # basic task list script
 # currently python 2.7
-# next prioritised changes - add file handling for data persistence, adjust style and flow to be object oriented
+# next prioritised changes - adjust style and flow to be object oriented
+# 0.5 - added file handling for saving data between sessions
 # 0.4 - main control flow moved to a function call, changed ifs to use boolean value of list
 # 0.3 - tasks changed from a list to dictionary, task name: todays date
 # 0.2 - option to remove tasks
@@ -8,12 +9,18 @@
 # 0.0 - basic list input
 
 from datetime import date
+import pickle
 
 print '*' * 15
 print "Task List Manager"
 print '*' * 15
 
-tasks = {}
+#import saved task list from file, create if doesn't exits
+pkl_file = open('myfile.pkl', 'wb+')
+tasks = pickle.load(pkl_file) 
+pkl_file.close()
+#tasks = {}
+
 user_input = ""
 
 def script_response(user_input):
@@ -41,6 +48,9 @@ def script_response(user_input):
 			print "No Tasks in list"
 	elif user_input == 'e':
 		exit
+		output = open('myfile.pkl', 'wb')
+		pickle.dump(tasks, output)
+		output.close()
 	else:
 		print "Invalid input, try again"
 
@@ -49,5 +59,6 @@ while user_input != 'e':
 	print "Instructions:"
 	user_input = raw_input("Press 'l' to list tasks, n' to add new, 'd' to delete or 'e' to exit\n.:")
 	script_response(user_input)
+	
 
 
