4893594535998f9f4a3f644078be78776045a56f
Chrijj <chrijj@gmail.com>
2014-05-30 23:02:49 +0100

    tabbed printing, file saving fixed

--- task_dot.py
+++ task_dot.py
@@ -1,6 +1,7 @@
 # basic task list script
 # currently python 2.7
-# next prioritised changes - adjust style and flow to be object oriented
+# next prioritised changes - adjust style and flow to be object oriented, improve readme and move revision history to there
+# 0.6 - adjusted printing of tasks to loop through all entries, indented machine responses to improve readability, fixed bug to correctly save pickled data to file
 # 0.5 - added file handling for saving data between sessions
 # 0.4 - main control flow moved to a function call, changed ifs to use boolean value of list
 # 0.3 - tasks changed from a list to dictionary, task name: todays date
@@ -29,36 +30,37 @@ def script_response(user_input):
 	if user_input == 'n':
 		user_input = raw_input("Enter the task: ")
 		tasks[user_input] = date.today()
-		print "Task", user_input, "added to task list\n"
+		print "\t Task", user_input, "added to task list\n"
 	elif user_input == 'd':
 		if tasks:
-			print "Current Tasks:\n",tasks
+			print "\t Current Tasks:\n",tasks
 			user_input = raw_input("Task to delete: ")
 			if user_input not in tasks:
-				print "That task is not in the list"
+				print "\t That task is does not exist."
 			else:
 				del tasks[user_input]
-				print user_input, "removed from tasks."
+				print "\t", user_input, "removed from tasks."
 		else:
-			print "No Tasks in list"
+			print "\t No Tasks in list"
 	elif user_input == 'l':
 		if tasks:
-			print tasks
+			sorted = tasks.items()
+			sorted.sort()
+			for key, value in sorted:
+				print "\t", value, key
 		else:
-			print "No Tasks in list"
+			print "\t No Tasks in list"
 	elif user_input == 'e':
-		exit
 		output = open('myfile.pkl', 'wb')
 		pickle.dump(tasks, output)
 		output.close()
+		exit
 	else:
-		print "Invalid input, try again"
+		print "\t Invalid input, try again"
 
 while user_input != 'e':
 	print '-' * 15
 	print "Instructions:"
 	user_input = raw_input("Press 'l' to list tasks, n' to add new, 'd' to delete or 'e' to exit\n.:")
 	script_response(user_input)
-	
-
 
